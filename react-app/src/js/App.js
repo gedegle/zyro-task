ddd081d0f914b8779d739a115c4e5e83f988e2f7
Eglė <gedgaudaite.egle@gmail.com>
2020-03-04 10:30:03 +0200

    Add dynamic colour changing on rows

--- react-app/src/js/App.js
+++ react-app/src/js/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { FaChevronDown } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { IconContext } from "react-icons";
+
 function NavBar() {
     return (
         <div className={"nav-bar-container"}>
@@ -44,6 +45,7 @@ function SideNav(props) {
             </aside>
         )
 }
+
 class App extends React.Component {
     constructor() {
         super();
@@ -56,22 +58,23 @@ class App extends React.Component {
 
         this.compareByDesc.bind(this);
         this.compareByAsc.bind(this);
-        this.sortBy.bind(this);
+        this.sortBy = this.sortBy.bind(this);
         this.onRowClick = this.onRowClick.bind(this);
     }
 
     receiveData() {
         const url = "https://api.themoviedb.org/3/discover/tv?api_key=d847b6fbf410655f6b71421e46883b4f&language=en-US&" +
-            "sort_by=popularity.desc&page="+this.state.pageNumber+
+            "sort_by=popularity.desc&page=" + this.state.pageNumber +
             "&timezone=America%2FNew_York&include_null_first_air_dates=false";
         let tempMovieArr = [];
-        axios.get(url,{ cancelToken: this.axiosCancelSource.token }).then( res => {
-            res.data.results.forEach((item)=>{
+        axios.get(url, {cancelToken: this.axiosCancelSource.token}).then(res => {
+            res.data.results.forEach((item) => {
                 tempMovieArr.push({
+                    "id": item.id,
                     "name": item.name,
                     "vote": item.vote_average,
                     "language": item.original_language,
-                    "date": item.first_air_date.slice(0,4),
+                    "date": item.first_air_date.slice(0, 4),
                     "popularity": item.popularity
                 });
             });
@@ -89,6 +92,7 @@ class App extends React.Component {
         this.axiosCancelSource = axios.CancelToken.source();
         this.receiveData();
     }
+
     componentWillUnmount() {
         this.axiosCancelSource.cancel('Component unmounted.')
     }
@@ -100,6 +104,7 @@ class App extends React.Component {
             return 0;
         };
     }
+
     compareByDesc(key) {
         return function (a, b) {
             if (a[key] > b[key]) return -1;
@@ -108,47 +113,86 @@ class App extends React.Component {
         };
     }
 
-    sortBy(key,e) {
+    sortBy(key, e) {
         e.preventDefault();
+        this.toggleClass(e,'fa-rotate-180', false);
 
         let arrayCopy = [...this.state.movies];
         arrayCopy.sort(this.compareByAsc(key));
 
-        if(JSON.stringify(arrayCopy)===JSON.stringify(this.state.movies))
+        if (JSON.stringify(arrayCopy) === JSON.stringify(this.state.movies))
             arrayCopy.sort(this.compareByDesc(key));
 
-        this.setState({movies: arrayCopy});
-        this.toggleClass(e);
+        this.setState({
+            movies: arrayCopy,
+            selectedSort: e.target.id,
+            prevSelSort: this.state.selectedSort
+        }, function () {
+            this.removeClasses(this.state.prevSelSort, this.state.selectedSort, false, 'fa-rotate-180');
+        });
+
+        this.retoggleRowColour();
     }
 
-    toggleClass(e) {
+    toggleClass(e, className, classTwo) {
         let target;
+        if(classTwo) {
+            target = e.target.parentNode;
+            target.childNodes[5].childNodes[0].classList.toggle(classTwo);
+        }
+        else target = e.target.childNodes[1].childNodes[0];
 
-        //toggles up-arrow class
-        e.target.childNodes[1].childNodes[0].classList.toggle('fa-rotate-180');
-        target = e.target.childNodes[1].childNodes[0];
+        target.classList.toggle(className);
+    }
 
-        //restores remaining arrows to it's default position
-        let cellClasses = document.getElementsByClassName('fa-rotate-180');
-        if(cellClasses) {
-            for (let i = 0; i < cellClasses.length; i++) {
-                if (cellClasses[i] !== target)
-                    cellClasses[i].classList.remove('fa-rotate-180');
+    removeClasses(prevSelected, selected, scndClass, className) {
+        let prevClass = document.getElementById(prevSelected);
+        if (prevClass) {
+            if(!scndClass && prevClass !== selected)
+                prevClass.childNodes[1].childNodes[0].classList.remove(className);
+            else if (scndClass) {
+                prevClass.childNodes[5].childNodes[0].classList.remove(scndClass);
+                prevClass.classList.remove(className);
             }
         }
     }
+
+    toggleClasses(selected, firstClass, scndClass) {
+        let target = document.getElementById(selected);
+
+        if(scndClass) {
+            target.classList.toggle(firstClass);
+            target.childNodes[5].childNodes[0].classList.toggle(scndClass);
+        } else {
+            target.childNodes[1].childNodes[0].classList.toggle(firstClass);
+        }
+    }
+    retoggleRowColour() {
+        this.removeClasses(this.state.selected, this.state.selected, 'icon--star--colour-white','row-on-focus' );
+        this.setState({
+            selected: this.state.selected
+        }, function () {
+            this.toggleClasses(this.state.selected, 'row-on-focus','icon--star--colour-white' );
+        })
+    }
+
     onRowClick(e) {
         e.preventDefault();
         let rowData = e.target.parentNode.childNodes;
 
         this.setState({
-            selectedName: rowData[0].innerText,
-            selectedLang: rowData[1].innerText,
-            selectedYear: rowData[2].innerText,
-            selectedPop: rowData[3].innerText,
-            selectedVote: rowData[4].innerText,
-            showSelection: true
-        })
+            selectedName: rowData[1].innerText,
+            selectedLang: rowData[2].innerText,
+            selectedYear: rowData[3].innerText,
+            selectedPop: rowData[4].innerText,
+            selectedVote: rowData[5].innerText,
+            showSelection: true,
+            selected: e.target.parentNode.id,
+            prevSelected: this.state.selected
+        }, function () {
+            this.toggleClasses(this.state.selected, 'row-on-focus','icon--star--colour-white' );
+            this.removeClasses(this.state.prevSelected, this.state.selected,'icon--star--colour-white', 'row-on-focus');
+        });
     }
     render() {
         return(
@@ -159,7 +203,16 @@ class App extends React.Component {
                 <table className={"content-table"}>
                     <thead className={"content-table__header"}>
                     <tr>
-                        <th className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('name',e)}>Name
+                        <th id={"id"} className={"content-table__header__cell"}>ID</th>
+                        <th id={"name"} className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('name',e)}>Name
+                            <IconContext.Provider
+                                value={{ pointerEvents: "none"}}>
+                                <span  className={"icon"}>
+                                    <FaChevronDown  />
+                                </span>
+                            </IconContext.Provider>
+                        </th>
+                        <th id={"lang"} className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('language',e)}>Language
                             <IconContext.Provider
                                 value={{ pointerEvents: "none"}}>
                                 <span className={"icon"}>
@@ -167,7 +220,7 @@ class App extends React.Component {
                                 </span>
                             </IconContext.Provider>
                         </th>
-                        <th className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('language',e)}>Language
+                        <th id={"date"} className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('date',e)}>Release date
                             <IconContext.Provider
                                 value={{ pointerEvents: "none"}}>
                                 <span className={"icon"}>
@@ -175,7 +228,7 @@ class App extends React.Component {
                                 </span>
                             </IconContext.Provider>
                         </th>
-                        <th className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('date',e)}>Release date
+                        <th id={"pop"} className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('popularity',e)}>Popularity
                             <IconContext.Provider
                                 value={{ pointerEvents: "none"}}>
                                 <span className={"icon"}>
@@ -183,15 +236,7 @@ class App extends React.Component {
                                 </span>
                             </IconContext.Provider>
                         </th>
-                        <th className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('popularity',e)}>Popularity
-                            <IconContext.Provider
-                                value={{ pointerEvents: "none"}}>
-                                <span className={"icon"}>
-                                    <FaChevronDown />
-                                </span>
-                            </IconContext.Provider>
-                        </th>
-                        <th className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('vote',e)}>IMDb vote
+                        <th id={"vote"} className={"content-table__header__cell icon-arrow"} onClick={(e) => this.sortBy('vote',e)}>IMDb vote
                             <IconContext.Provider
                                 value={{ pointerEvents: "none"}}>
                                 <span className={"icon"}>
@@ -203,7 +248,8 @@ class App extends React.Component {
                     </thead>
                     <tbody className={"content-table__body"}>
                     {this.state.movies.length > 0 && this.state.movies.map((item, i) => (
-                        <tr key={i} className={"content-table__body__row"} onClick={this.onRowClick}>
+                        <tr key={i} id={item.id} className={"content-table__body__row"} onClick={this.onRowClick}>
+                            <td className={"content-table__body__cell content-table__body__cell--small-number"}>{item.id}</td>
                             <td className={"content-table__body__cell content-table__body__cell--bold"}>{item.name}</td>
                             <td className={"content-table__body__cell"}>{item.language}</td>
                             <td className={"content-table__body__cell"}>{item.date}</td>
